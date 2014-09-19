abee1b0eb5a5d48fd613788a9c7bbaa529d39080
Daniel Tea <daniel.l.tea@gmail.com>
2014-09-19 12:44:33 -0700

    part 11 almost complete

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,25 +1,34 @@
-//$( document ).ready(function() {
-//
-//  //
-//
-//});
-
-
 $(function() {
-  smoothScroll(300);
+  smoothScrool(300);
+  workBelt();
 });
 
-function smoothScroll (duration) {
+
+function smoothScrool (duration) {
   $('a[href^="#"]').on('click', function(event) {
-    
-    var target = $( $(this).attr('href') );
 
-    if( target.length ) {
-      event.preventDefault();
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, duration); 
-    }
+      var target = $( $(this).attr('href') );
 
+      if( target.length ) {
+          event.preventDefault();
+          $('html, body').animate({
+              scrollTop: target.offset().top
+          }, duration);
+      }
   });
+}
+
+
+function workBelt() {
+
+  $('.thumb-unit').click(function() {
+    $('.work-belt').css('left','-100%');
+    $('.work-container').show();
+  });
+  
+  $('.work-return').click(function() {
+    $('.work-belt').css('left','0%');
+    $('.work-container').hide(800);
+  });
+
 }
