56233d5a45d924ddc8bd7e4fa2179b14058e3739
Daniel Tea <daniel.l.tea@gmail.com>
2014-09-19 09:57:21 -0700

    updated all files

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,25 @@
-$( document ).ready(function() {
+//$( document ).ready(function() {
+//
+//  //
+//
+//});
 
-  //
 
+$(function() {
+  smoothScroll(300);
 });
+
+function smoothScroll (duration) {
+  $('a[href^="#"]').on('click', function(event) {
+    
+    var target = $( $(this).attr('href') );
+
+    if( target.length ) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, duration); 
+    }
+
+  });
+}
